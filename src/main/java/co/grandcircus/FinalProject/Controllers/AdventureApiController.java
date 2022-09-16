6d426829c12a9bf0fd6079e7166f6f7e09b9d957
Heather Swank <hdamoth@gmail.com>
2022-09-16 13:42:53 -0400

    added api functionality

--- src/main/java/co/grandcircus/FinalProject/Controllers/AdventureApiController.java
+++ src/main/java/co/grandcircus/FinalProject/Controllers/AdventureApiController.java
@@ -8,8 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import co.grandcircus.FinalProject.HelperFunctions.SceneID;
-import co.grandcircus.FinalProject.HelperFunctions.StoryID;
+
 
 import co.grandcircus.FinalProject.Models.Scene;
 import co.grandcircus.FinalProject.Models.Story;
